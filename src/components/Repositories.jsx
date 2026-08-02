a5c1919e55a9b0dc1cffad98c6802a5f4ca36fed
par jin sung <parkgutime@gmail.com>
2026-08-02 19:53:35 +0900

    fix: align AICC portfolio card height

--- src/components/Repositories.jsx
+++ src/components/Repositories.jsx
@@ -147,7 +147,7 @@ const Repositories = () => {
       <div className="contents">
         <div className="repositoriesBox">
           <Swiper modules={[Navigation, Pagination]} navigation pagination={{ clickable: true }} slidesPerView={1}>
-            <SwiperSlide className="repository">
+            <SwiperSlide className="repository aiccRepository">
               <li className="projectDesc">
                 <p className="repoTitle">Repositories</p>
                 <div className="projectNameRow">
